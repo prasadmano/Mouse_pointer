305a13674b6205b852c778aabcb9b03a363f027e
Prasad Mano <69799970+prasadmano@users.noreply.github.com>
2021-09-12 11:04:43 +0530

    fps & hand display
    function to be displayed on screen

--- mp.py
+++ mp.py
@@ -1,28 +1,21 @@
 import cv2
 import mediapipe as mp
 import time
+import viewing
 
+# if frames per second is needed put fpsdisplay to True
+FPSDISPLAY = True
+HANDLANDMARK = True
 
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
+DISPLAY = viewing.display_annotation()
+
 mp_hands = mp.solutions.hands
-
-
-prev_frame_time = 0
-new_frame_time = 0
-font = cv2.FONT_HERSHEY_SIMPLEX
 
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as hands:
   while cap.isOpened():
-    new_frame_time = time.time()
-    fps = 1/(new_frame_time-prev_frame_time)
-    prev_frame_time = new_frame_time
-    fps = int(fps)
-    fps = str(fps)
-
     success, image = cap.read()
     if not success:
       print("Ignoring empty camera frame.")
@@ -32,6 +25,7 @@ with mp_hands.Hands(
     # Flip the image horizontally for a later selfie-view display, and convert
     # the BGR image to RGB.
     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+
     # To improve performance, optionally mark the image as not writeable to
     # pass by reference.
     image.flags.writeable = False
@@ -42,21 +36,16 @@ with mp_hands.Hands(
     # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_hand_landmarks:
-      #print('-------------------------------------------------------------------------')
-      #print(results.multi_hand_landmarks)
 
-      for hand_landmarks in results.multi_hand_landmarks:
-        #print(f'Index finger tip coordinate: (',f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, 'f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_hight})')
+    # for fps on screen
+    if FPSDISPLAY:
+        image = DISPLAY.fps(image)
 
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
-    cv2.putText(image, fps, (7, 70), font, 1, (100, 255, 0), 3, cv2.LINE_AA)
-    cv2.circle(image, (20,100), radius=2, color=(100, 255, 0), thickness=2)
+    # for hand_landmarks on screen
+    if ((results.multi_hand_landmarks != None) and (HANDLANDMARK == True)) :
+        DISPLAY.hand_landmarks(image, results.multi_hand_landmarks)
+
+    #cv2.circle(image, (20,100), radius=2, color=(100, 255, 0), thickness=2)
     cv2.imshow('MediaPipe Hands', image)
 
     if cv2.waitKey(5) & 0xFF == 27:
---
+++ viewing.py
@@ -0,0 +1,39 @@
+import cv2
+import mediapipe as mp
+import time
+
+class display_annotation:
+    """display_annotation class is for displaying fps and other annotations from
+    displaying on screen"""
+    prev_frame_time = 0
+    new_frame_time = 0
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+
+    def __init__(self):
+        #self.arg = arg
+        pass
+
+    def fps(self, image):
+        """display fps on display"""
+        self.image = image
+        display_annotation.new_frame_time = time.time()
+        fps = 1/(display_annotation.new_frame_time-display_annotation.prev_frame_time)
+        display_annotation.prev_frame_time = display_annotation.new_frame_time
+        fps = int(fps)
+        fps = str(fps)
+        return cv2.putText(self.image, fps, (7, 70), display_annotation.font, 1, (100, 255, 0), 3, cv2.LINE_AA)
+
+    def hand_landmarks(self, image, multi_hand_landmarks):
+        """display hand_landmarks on display"""
+        self.image = image
+        self.multi_hand_landmarks = multi_hand_landmarks
+
+        for hand_landmarks in multi_hand_landmarks:
+          return display_annotation.mp_drawing.draw_landmarks(
+              image,
+              hand_landmarks,
+              display_annotation.mp_hands.HAND_CONNECTIONS,
+              display_annotation.mp_drawing_styles.get_default_hand_landmarks_style(),
+              display_annotation.mp_drawing_styles.get_default_hand_connections_style())
